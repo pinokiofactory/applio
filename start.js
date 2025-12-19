fec8a88f3c934b766c5856719ca437494c4decbd
6Morpheus6 <wanton-decay@gmx.net>
2025-12-19 05:27:23 +0100

    update
    remove local sharing

--- start.js
+++ start.js
@@ -28,13 +28,6 @@ module.exports = {
       params: {
         url: "{{input.event[0]}}"
       }
-    },
-    {
-      method: "proxy.start",
-      params: {
-        uri: "{{local.url}}",
-        name: "Local Sharing"
-      }
     }
   ]
 }
